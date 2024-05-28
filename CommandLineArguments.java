8d7d773ba6fa7b36217ea15343c610b77710431b
Aryan Barde <147256003+aryanbarde80@users.noreply.github.com>
2024-05-28 09:20:10 +0530

    Update CommandLineArguments.java

--- CommandLineArguments.java
+++ CommandLineArguments.java
@@ -2,6 +2,16 @@ public class CommandLineArguments {
     public static void main(String[] args) {
         for(String t:args){
             System.out.println(t);
+
+            //To run this code 
+            /*
+            Command 1 : javac CommandLineArguments.java
+            Command 2: java CommandLineArguments Hello World
+
+            This Prints:-
+                Hello
+                World
+            */
         }
     }
     
