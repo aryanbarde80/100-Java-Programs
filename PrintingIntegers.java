5798dce93ce77f82e25ecd8d7530b84a7fae5f5b
Aryan Barde <147256003+aryanbarde80@users.noreply.github.com>
2024-05-28 09:10:30 +0530

    (28/05/2024)

---
+++ PrintingIntegers.java
@@ -0,0 +1,11 @@
+public class PrintingIntegers {
+    public static void main(String[] args) {
+        int c; //Declaring a Variable
+
+        /*Using loop to repart instruction execution */
+
+        for(c=1;c<=10;c++){
+            System.out.println(c);
+        }
+    }
+}
